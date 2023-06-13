d4d788854947a156fbbfe29c813c4803b0fc2714
Your Name <you@example.com>
2023-06-13 15:11:03 +0200

    js tuts

---
+++ if-statements.js
@@ -0,0 +1,26 @@
+let a= 12;
+let b =13;
+
+//if
+if (a == b )
+{
+    console.log("true");
+}else{
+    console.log("false");
+}
+
+for(i = 0; i<= 10; i++)
+{
+    console.log(i);
+}
+
+
+//ToDo
+//array.forEach(element => {
+    
+//});
+
+
+while(false){
+    console.log("hi");
+}   
