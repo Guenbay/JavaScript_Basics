b2250700a0253e0a41352816c391ba8bff7a981b
Günbay Sali <gunbayx@hotmail.de>
2023-06-21 16:23:12 +0200

    new lines insert

--- training130623/1variable.js
+++ training130623/1variable.js
@@ -1,6 +1,9 @@
 // Erstelle eine Variable mit dem Namen name und weise ihr deinen eigenen Namen als Wert zu. 
 //Gib den Wert der Variablen in der Konsole aus.
 
+//variable name definieren 
 var name = "Günbay Sali";
+
+//ausgabe mit console.log(name);
 console.log(name);
 
